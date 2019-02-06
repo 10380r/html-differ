54e87470e3437af32a6c3606e4098a6af06e5e54
Tomiyama <10380r@gmail.com>
2019-02-06 21:57:11 +0900

    fix

--- file_diff.py
+++ file_diff.py
@@ -1,27 +1,31 @@
 import datetime
 import difflib
 
-#今日の日付を取得
-today = datetime.date.today()
+def print_diff(today_html, yesterday_html):
+    #HTMLファイルの差分の解析
+    result = difflib.Differ().compare(today_html, yesterday_html)
+    #HTMLファイルの差分のみを出力
+    for i in list(result):
+        if "+" or "-"in i:
+            print(i)
 
-#昨日の日付を取得
-yesterday = today - datetime.timedelta(days=1)
+def main():
+    #今日の日付を取得
+    today = datetime.date.today()
+    #昨日の日付を取得
+    yesterday = today - datetime.timedelta(days=1)
 
-#今日のHTMLファイルの読み込み
-today_file = 'src/{}.html'.format(str(today))
-with open(today_file, 'r') as today_f:
-    today = today_f.readlines()
+    #今日のHTMLファイルの読み込み
+    today_file = 'src/{}.html'.format(str(today))
+    with open(today_file, 'r', encoding='shift-jis') as today_f:
+        today_html = today_f.readlines()
+    
+    #昨日のHTMLファイルの読み込み
+    yesterday_file = 'src/{}.html'.format(str(yesterday))
+    with open (yesterday_file,'r', encoding='shift-jis') as yesterday_f:
+        yesterday_html = yesterday_f.readlines()
 
-#昨日のHTMLファイルの読み込み
-yesterday_file = 'src/{}.html'.format(str(yesterday))
-with open (yesterday_file,'r') as yesterday_f:
-    yesterday = yesterday_f.readlines()
+    print_diff(today_html, yesterday_html)
 
-#HTMLファイルの差分の解析
-result = difflib.Differ().compare(t, y)
-diff = list(result)
-
-#HTMLファイルの差分のみを出力
-for i in diff:
-    if "+" or "-"in i:
-        print(i)
+if __name__ == '__main__':
+    main()
--- get_html.py
+++ get_html.py
@@ -3,20 +3,26 @@ from bs4 import BeautifulSoup
 import datetime
 import codecs
 
-#指定するURL
-url = "https://www.cinemanavi.com/ranking_list/type/favorite/%E6%98%A0%E7%94%BB%E3%83%A9%E3%83%B3%E3%82%AD%E3%83%B3%E3%82%B0.html"
+def get_html(url):
+    # URLからHTTPレスポンスを取得
+    html = BeautifulSoup(urllib.request.urlopen(url), "html.parser")
+    return html
 
-#今日の日付の取得
-today = datetime.date.today()
+def save_as_file(html):
+    #今日の日付の取得
+    today = datetime.date.today()
+    #ファイル名の指定
+    file_name = 'src/{}.html'.format(str(today))
+    #ファイルをつくる
+    file = codecs.open(file_name,"ab",'cp932', 'ignore')
+    file.write(html)
+    file.close()
 
-# URLからHTTPレスポンスを取得
+def main():
+    #指定するURL
+   # url = "https://www.cinemanavi.com/ranking_list/type/favorite/%E6%98%A0%E7%94%BB%E3%83%A9%E3%83%B3%E3%82%AD%E3%83%B3%E3%82%B0.html"
+    url = 'https://docs.python.jp/3/library/difflib.html'
+    save_as_file(get_html(url))
 
-html= BeautifulSoup(urllib.request.urlopen(url), "html.parser")
-
-#ファイル名の指定
-file_name = 'src/{}.html'.format(str(today))
-
-#ファイルをつくる
-file = codecs.open(file_name,"ab",'cp932', 'ignore')
-file.write(html)
-file.close()
+if __name__ == '__main__':
+    main()
